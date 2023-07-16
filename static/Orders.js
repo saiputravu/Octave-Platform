f62fe6b41ad236318d8126a026c959cfbd9e60fc
sai.putravu <sai.putravu@student.manchester.ac.uk>
2023-07-16 18:12:31 +0100

    Added ananda files

---
+++ static/Orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Tesla',
+        productNumber: 'Dividend',
+        paymentStatus: '16 August 2023 | 14:00 (US)',
+        status: 'Completed'
+    },
+    {
+        productName: 'Google',
+        productNumber: 'Dividend',
+        paymentStatus: '17 August 2023 | 14:00 (US)',
+        status: 'Pending'
+    },
+    {
+        productName: 'Morningstar',
+        productNumber: 'Dividend',
+        paymentStatus: '17 August 2023 | 14:00 (US)',
+        status: 'Pending'
+    }
+]
